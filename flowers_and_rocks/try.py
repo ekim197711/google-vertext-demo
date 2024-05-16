d4daba87e4c9d513a40a25171790b095ca8bb45f
Mike Møller Nielsen <ekim197711@gmail.com>
2024-05-16 14:31:20 +0200

    imgs

---
+++ flowers_and_rocks/try.py
@@ -0,0 +1,7 @@
+
+predict_image_classification_sample(
+    project="540600113697",
+    endpoint_id="1172719310976581632",
+    location="us-central1",
+    filename="YOUR_IMAGE_FILE"
+)
---
+++ flowers_and_rocks/try_rock.sh
@@ -0,0 +1,10 @@
+ENDPOINT_ID="1172719310976581632"
+PROJECT_ID="540600113697"
+INPUT_DATA_FILE="input_data.json"
+
+curl \
+-X POST \
+-H "Authorization: Bearer $(gcloud auth print-access-token)" \
+-H "Content-Type: application/json" \
+https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/endpoints/${ENDPOINT_ID}:predict \
+-d "@${INPUT_DATA_FILE}"
