f9294097de8d08e20d498bbf770bc70eddcaf344
Mike Nielsen <mmni@protonmail.com>
2024-05-21 22:57:37 +0200

    python stuff

---
+++ python/src/main.py
@@ -0,0 +1,9 @@
+from src.util.predict_image_classification_sample import predict_image_classification_sample
+
+if __name__ == "__main__":
+    predict_image_classification_sample(
+        project="540600113697",
+        endpoint_id="1172719310976581632",
+        location="us-central1",
+        filename="rock_on_grass.png"
+    )
